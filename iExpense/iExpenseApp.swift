7c8db13d96bb049c1a1d7fa58298710753c6e0d8
Masoud Safari <mmsafari@gmail.com>
2024-02-29 19:30:52 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Masoud Safari on 2024-02-29.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
