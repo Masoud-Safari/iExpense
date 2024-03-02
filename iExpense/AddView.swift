65aaeb139822bc2f613720d7ea1ff8108bda0725
Masoud Safari <mmsafari@gmail.com>
2024-03-01 16:01:17 -0800

    Add AddView view to get information from user.

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+    
+    let types = ["Business", "Personal"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "usd"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
     }
 }
 
 #Preview {
-    AddView()
+    AddView(expenses: Expenses())
 }
