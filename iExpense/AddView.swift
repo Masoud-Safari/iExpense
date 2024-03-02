86f4cafc439e6882a73da72f16ac33c0b53217eb
Masoud Safari <mmsafari@gmail.com>
2024-03-01 16:00:28 -0800

    Add AddView view to get information from user.

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Masoud Safari on 2024-03-01.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
